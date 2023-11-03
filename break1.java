732a8673e867a9b5e1431afc7388318d600cc420
gallianceb <149685053+gallianceb@users.noreply.github.com>
2023-11-03 04:34:08 -0700

    Add files via upload

---
+++ break1.java
@@ -0,0 +1,12 @@
+class break1 {
+    public static void main(String args[]){
+        for(int n=10;n<50;n++){
+            if(n==40){
+                break;
+            }
+            System.out.println(n);
+        }
+    }
+}
+    
+
