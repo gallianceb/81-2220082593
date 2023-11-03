9d974fd7bba6422328dcd183e0ee746b5c2d59a5
gallianceb <149685053+gallianceb@users.noreply.github.com>
2023-11-03 04:42:02 -0700

    Add files via upload

---
+++ nested2.java
@@ -0,0 +1,10 @@
+class nested2{
+    public static void main(String args[]){
+        for( int i=9;i>=0;i--){
+            for(int k=1;k<i;k++){
+                System.out.print("g");
+            }
+            System.out.println();
+        }
+    }
+}
